e450a08b8fb9c1cedcfe04111b091a79e3d8f11b
friend8 <map@wafriv.de>
2015-04-25 23:23:17 +0200

    fix the paths in the db migrations management file

--- db_migrations/migrations.php
+++ db_migrations/migrations.php
@@ -6,9 +6,9 @@
  * @author  friend8 <map@wafriv.de>
  * @license https://www.gnu.org/licenses/lgpl.html LGPLv3
  */
-require_once(__DIR__.'/../lib/config.default.php');
-require_once(__DIR__.'/../lib/util/mysql.php');
-require_once(__DIR__.'/../lib/util/general.php');
+require_once(__DIR__.'/../config.default.php');
+require_once(__DIR__.'/../util/mysql.php');
+require_once(__DIR__.'/../util/general.php');
 
 $result = migration_manager($_REQUEST);
 
