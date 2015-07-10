d885adf2224b1cf49302b052289396589fd2a0df
friend8 <map@wafriv.de>
2015-07-11 01:02:56 +0200

    add missing copyright for the file doc blocks

--- Classes/Display.php
+++ Classes/Display.php
@@ -1,10 +1,24 @@
 <?php
 /**
- * Part of the SmartWork framework.
+ * This file is part of SmartWork.
  *
- * @package SmartWork
- * @author  friend8 <map@wafriv.de>
- * @license https://www.gnu.org/licenses/lgpl.html LGPLv3
+ * SmartWork is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SmartWork is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with SmartWork.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package   SmartWork
+ * @author    Marian Pollzien <map@wafriv.de>
+ * @copyright (c) 2015, Marian Pollzien
+ * @license   https://www.gnu.org/licenses/lgpl.html LGPLv3
  */
 namespace SmartWork;
 
@@ -12,7 +26,7 @@ namespace SmartWork;
  * Handles the displaying of pages.
  *
  * @package SmartWork
- * @author  friend8 <map@wafriv.de>
+ * @author  Marian Pollzien <map@wafriv.de>
  * @license https://www.gnu.org/licenses/lgpl.html LGPLv3
  */
 class Display
